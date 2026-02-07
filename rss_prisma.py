ae1c590ecc411f207ea44cc9a8f82162e5b0c10b
prismanews <ovalero@gmail.com>
2026-02-07 14:48:24 +0100

    Update rss_prisma.py

--- rss_prisma.py
+++ rss_prisma.py
@@ -179,7 +179,7 @@ html = f"""
 <head>
 <meta charset="UTF-8">
 <title>Prisma</title>
-<link rel="stylesheet" href="prisma.css">
+<link rel="stylesheet" href="prisma.css?v={datetime.now().timestamp()}">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
